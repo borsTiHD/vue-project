e2042b9681a4db2aef0b0a630c50964fb5556019
borsTiHD <bastian.j@gmx.net>
2022-08-13 13:58:02 +0200

    Main page content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <template>
     <main>
-        main
+        <Card class="my-2 bg-white dark:bg-zinc-800 rounded-lg ring-1 ring-slate-900/5 shadow-xl dark:shadow-md dark:text-zinc-400">
+            <template #title>
+                This is the main page
+            </template>
+            <template #content>
+                <div class="flex-col">
+                    <p>Main text</p>
+                </div>
+            </template>
+        </Card>
     </main>
 </template>
 
