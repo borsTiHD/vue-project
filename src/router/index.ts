0cbce1a91892d3833855d8e50cf60fa1782b8268
borsTiHD <bastian.j@gmx.net>
2022-08-14 10:03:09 +0200

    App >> Changing Head Title on route change.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { useAppStore } from '@/stores/app'
+import HomeView from '@/views/HomeView.vue'
 
+// Router
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
             name: 'home',
-            component: HomeView
+            component: HomeView,
+            meta: { title: 'Home' }
         },
         {
             path: '/about',
@@ -15,9 +18,17 @@ const router = createRouter({
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/AboutView.vue')
+            component: () => import('../views/AboutView.vue'),
+            meta: { title: 'About' }
         }
     ]
+})
+
+// Changing title after route change
+router.afterEach((to) => {
+    const appStore = useAppStore()
+    const pageTitle = `${to.meta.title} - ${appStore.getTitle}`
+    document.title = pageTitle || appStore.getTitle
 })
 
 export default router
